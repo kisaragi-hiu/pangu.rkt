0e2a0564d31db8cd34a30f76f8fa239fecea81e5
Kisaragi Hiu <mail@kisaragi-hiu.com>
2019-12-12 07:25:54 +0900

    forgot to provide

--- pangu/main.rkt
+++ pangu/main.rkt
@@ -2,6 +2,10 @@
 ;; Paranoid text spacing in Racket
 ;; Port of pangu.py: https://github.com/vinta/pangu.py
 
+(provide spacing
+         spacing-text
+         spacing-file)
+
 ;;; Regex definitions
 (define cjk "\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30fa\u30fc-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff")
 
